113924fd296d9235119f9696427d124bcd56c2cf
James <james@jeclark.co.uk>
2021-03-31 21:04:18 +0100

    Additional CSS to Layout module

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { container, heading } from './layout.module.css';
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
   return (
@@ -9,12 +15,16 @@ const Layout = ({ pageTitle, children }) => {
       <h1 className={heading}>{pageTitle}</h1>
       {children}
       <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to='/' className={navLinkText}>
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to='/about'>About</Link>
+          <li className={navLinkItem}>
+            <Link to='/about' className={navLinkText}>
+              About
+            </Link>
           </li>
         </ul>
       </nav>

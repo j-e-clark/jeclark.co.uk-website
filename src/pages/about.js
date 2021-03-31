3164defd9135b3d1acadbbe9a1aca3eb5e605983
James <james@jeclark.co.uk>
2021-03-31 13:09:01 +0100

    Customise content

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>My name is James.</p>
+      <Link to='/'>Home</Link>
+    </main>
+  );
+};
+
+export default AboutPage;

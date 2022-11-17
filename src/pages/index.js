ff9361326b7468f056d673c41b347ef594fae272
James <james@jeclark.co.uk>
2022-11-17 17:03:02 +0000

    Test a fresh Gatsby starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,5 @@
 import * as React from 'react';
-import Layout from '../components/layout';
-import { Seo } from '../components/seo';
 
-const IndexPage = () => {
-  return (
-    <Layout pageTitle='Home Page'>
-      <h2>
-        JEClark.co.uk
-        <br />
-        <span role='img' aria-label='Lightning bolt emojis'>
-          ⚡️⚡️⚡️
-        </span>
-        <span>Something amazing is coming</span>
-        <span role='img' aria-label='Lightning bolt emojis'>
-          ⚡️⚡️⚡️
-        </span>
-      </h2>
-    </Layout>
-  );
-};
-
-export default IndexPage;
-
-export const Head = () => <Seo />;
+export default function Home() {
+  return <div>Hello world!</div>;
+}

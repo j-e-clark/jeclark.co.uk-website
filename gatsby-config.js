e35ed86ed981e11056828ec5c8e664cdd4b3d9d5
James <james@jeclark.co.uk>
2022-11-17 17:22:05 +0000

    Install required packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,28 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  siteMetadata: {
+    title: 'JEClark.co.uk',
+    description: 'A simple homepage built with Gatsby',
+    twitterUsername: '@jeclark',
+    image: '/jeclark_logo.png',
+    imageAlt: 'JEClark.co.uk',
+    domain: 'jeclark.co.uk',
+    siteUrl: 'https://jeclark.co.uk',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `James Clark`,
+        short_name: `JEClark`,
+        start_url: `/`,
+        icon: `src/images/icon.svg`,
+        background_color: `#000000`,
+        theme_color: `#4176C0`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+  ],
+};
